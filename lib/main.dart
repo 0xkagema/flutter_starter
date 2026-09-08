1db11584ecdb83d4bcfd7de549674388cd536db3
0xkagema <reecejames934@gmail.com>
2026-09-08 10:21:43 +0300

    feat: implement signIn method to authenticate users with API client

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:toastification/toastification.dart';
 
 import 'app.dart';
+import 'core/http/client.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await ApiClient.instance.init();
   runApp(
     ProviderScope(
       child: ToastificationWrapper(
